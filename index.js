d53036181925d63d4d7332c904cee10af51073ee
ankihg <hagelin.annika@gmail.com>
2018-01-27 17:51:53 -0800

    horsin around

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ const fs = require('fs');
 const START = '[start]';
 const END = '[end]';
 
-fs.readFile('./text.txt', (err, contents) => {
+fs.readFile('./zanreads.txt', (err, contents) => {
     // console.log(contents.toString());
 
-    var _2gramCounts = natural.NGrams.ngrams(contents.toString(), 2, START, END)
+    var _2gramCounts = natural.NGrams.ngrams(contents.toString().toLowerCase(), 2, START, END)
         .reduce((hash, _3gram) => {
             hash[_3gram.toString()] = hash[_3gram.toString()] || 0;
             hash[_3gram.toString()]++;
             return hash;
         }, {});
 
-    var _3gramCounts = natural.NGrams.ngrams(contents.toString(), 3, START, END)
+    console.log(natural.NGrams.ngrams(contents.toString().toLowerCase(), 3, START, END));
+
+    var _3gramCounts = natural.NGrams.ngrams(contents.toString().toLowerCase(), 3, START, END)
         .reduce((hash, gram) => {
             hash[gram.toString()] = hash[gram.toString()] || 0;
             hash[gram.toString()]++;
@@ -34,10 +36,10 @@ fs.readFile('./text.txt', (err, contents) => {
         hash[_3gram[0]].push({prob: _3gramProbs[_3gramKey], ngram: _3gram});
         return hash;
     }, {});
-    // console.log('_3gramsByStart', _3gramsByStart);
+    // console.log('_3gramsByStart', JSON.stringify(_3gramsByStart, null, 4));
+    fs.writeFile('./probs.json', JSON.stringify(_3gramsByStart, null, 4), (err) => { err && console.log(err);})
 
-    console.log(_generate(_3gramsByStart));
-
+    // console.log(_generate(_3gramsByStart));
 });
 
 
