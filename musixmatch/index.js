77923b7d8ca353447e53835bfd6a9118a1b6aaa8
ankihg <hagelin.annika@gmail.com>
2018-04-20 21:25:07 -0700

    musixmatcvh config

---
+++ musixmatch/index.js
@@ -0,0 +1,9 @@
+const CONFIG = require('./config');
+const music = require('musicmatch')({usertoken:"",format:"",appid: CONFIG.APP_ID});
+
+music.track({track_id:15445219})
+.then(function(data){
+        console.log(data);
+}).catch(function(err){
+        console.log(err);
+})
