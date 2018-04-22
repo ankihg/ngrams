f6eb251b0bb90f9dc6dcfed88c0183e3fe099657
ankihg <hagelin.annika@gmail.com>
2018-04-21 19:04:18 -0700

    tweet error logging

--- twitter/index.js
+++ twitter/index.js
@@ -3,7 +3,8 @@ const T = require('./init')
 module.exports = {
     post(status) {
         T.post('statuses/update', { status: status }, function(err, data, response) {
-            console.log(response, !!err, data)
+            console.log(response.statusCode === '200' ? '\n####SUCCESS' : '\n####ERROR')
+            if (err) console.log(err);
         })
     }
 }
